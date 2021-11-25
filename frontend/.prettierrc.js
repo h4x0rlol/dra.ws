3975dd690e987ad16f655e8e4ca3400cc40f84f5
h4x0rlol <mazafaker.com@gmail.com>
2021-11-25 23:08:05 +0600

    feat: boilerplate setup

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	singleQuote: true,
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	jsxSingleQuote: false,
+};
