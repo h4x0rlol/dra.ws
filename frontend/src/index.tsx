51b5e7a773e6003b69e0e9afeef29e694d1fa53b
h4x0rlol <mazafaker.com@gmail.com>
2021-12-02 21:59:17 +0600

    feat: setup project base structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'src/styles/index.scss';
+import 'src/i18n';
 import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Suspense fallback={<div>Loading... </div>}>
+			<App />
+		</Suspense>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
