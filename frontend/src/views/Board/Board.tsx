e7906ed057741e3d1765abf21d2e8cb909b577a6
h4x0rlol <mazafaker.com@gmail.com>
2022-05-18 21:02:02 +0600

    chore: move to css modules

--- frontend/src/views/Board/Board.tsx
+++ frontend/src/views/Board/Board.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Canvas from 'src/components/Board/Canvas';
+import Canvas from 'src/components/Board/Canvas/Canvas';
 import SettingsBar from 'src/components/Board/SettingsBar';
 import Toolbar from 'src/components/Board/Toolbar';
-import './Board.scss';
+import styles from './Board.module.scss';
 
 const Board: React.FC = (): JSX.Element => {
 	return (
-		<div className="board">
+		<div className={styles.container}>
 			<SettingsBar />
 			<Toolbar />
 			<Canvas />
