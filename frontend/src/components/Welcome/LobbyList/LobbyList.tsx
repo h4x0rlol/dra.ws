ea030622c09e27de6509113f2a5b6696f4bede0e
h4x0rlol <mazafaker.com@gmail.com>
2022-06-02 02:01:25 +0600

    feat: add lobbylist comp

--- frontend/src/components/Welcome/LobbyList/LobbyList.tsx
+++ frontend/src/components/Welcome/LobbyList/LobbyList.tsx
@@ -1,9 +1,29 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './LobbyList.module.scss';
 
 const LobbyList: React.FC = (): JSX.Element => {
-	return <div>LobbyList</div>;
+	const { t } = useTranslation();
+	return (
+		<div className={styles.wrapper}>
+			<ul>
+				<li className={styles.item}>
+					<a href="dsa" className={styles.link}>
+						dsadada
+					</a>
+				</li>
+				<li className={styles.item}>
+					<a href="dsa" className={styles.link}>
+						dsadada
+					</a>
+				</li>
+			</ul>
+		</div>
+		// <div className={styles.empty}>
+		// 	<div>{t('home.empty')}</div>
+		// </div>
+	);
 };
 
 export default observer(LobbyList);
--- frontend/src/components/Welcome/Welcome.tsx
+++ frontend/src/components/Welcome/Welcome.tsx
@@ -1,10 +1,15 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import CreateLobby from './CreateLobby/CreateLobby';
+import LobbyList from './LobbyList/LobbyList';
 import styles from './Welcome.module.scss';
 
 const Welcome: React.FC = (): JSX.Element => {
-	return <div>Welcome</div>;
+	return (
+		<div>
+			<LobbyList />
+		</div>
+	);
 };
 
 export default observer(Welcome);
