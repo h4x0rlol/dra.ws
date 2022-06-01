3ecc00c6d940444f2a8f56b3f6db74bb92310159
h4x0rlol <mazafaker.com@gmail.com>
2022-06-02 02:08:08 +0600

    feat: add lobbycontainer comp

--- frontend/src/components/Welcome/LobbyContainer/LobbyContainer.tsx
+++ frontend/src/components/Welcome/LobbyContainer/LobbyContainer.tsx
@@ -1,9 +1,43 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import cn from 'classnames';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './LobbyContainer.module.scss';
+import CreateLobby from '../CreateLobby/CreateLobby';
+import LobbyList from '../LobbyList/LobbyList';
 
 const LobbyContainer: React.FC = (): JSX.Element => {
-	return <div>LobbyContainer</div>;
+	const { t } = useTranslation();
+	const [isCreateLobby, setIsCreateLobby] = useState(true);
+
+	return (
+		<>
+			<div className={styles.options}>
+				<button
+					type="button"
+					className={cn(styles.option, {
+						[styles.active]: isCreateLobby,
+						[styles.inactive]: !isCreateLobby,
+					})}
+					onClick={() => setIsCreateLobby(true)}
+				>
+					{t('home.create')}
+				</button>
+
+				<button
+					type="button"
+					className={cn(styles.option, {
+						[styles.active]: isCreateLobby,
+						[styles.inactive]: !isCreateLobby,
+					})}
+					onClick={() => setIsCreateLobby(false)}
+				>
+					{t('home.list')}
+				</button>
+			</div>
+			{isCreateLobby ? <CreateLobby /> : <LobbyList />}
+		</>
+	);
 };
 
 export default observer(LobbyContainer);
