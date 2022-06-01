f0950ccff25a00d615fa52a78b5f5a8b6df09987
h4x0rlol <mazafaker.com@gmail.com>
2022-06-01 20:36:59 +0600

    feat: add users list

--- frontend/src/components/Chat/UsersList/UsersList.tsx
+++ frontend/src/components/Chat/UsersList/UsersList.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styles from './UsersList.module.scss';
 
 const UsersList: React.FC = (): JSX.Element => {
-	return <div>UsersList</div>;
+	const { t } = useTranslation();
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.welcome}>{t('room.users')}</div>
+			<div className={styles.wrapper}>
+				<ul className={styles.list}>
+					<li className={styles.item}>Ultra_cool_name</li>
+					<li className={styles.item}>Ultra_cool_name</li>
+					<li className={styles.item}>Ultra_cool_name</li>
+					<li className={styles.item}>Ultra_cool_name</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default UsersList;
